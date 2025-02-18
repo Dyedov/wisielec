ec28ab0229525f0ae20b86999397bb94fcd5473e
Dmytro <dmytrodyedov@gmail.com>
2025-02-18 14:01:32 +0100

    test commit

--- wisielec_kod.py
+++ wisielec_kod.py
@@ -1,1 +1,40 @@
-print('hello git')
+"""
+1. musimy podać hasło
+2. musimy to hasło wyświetlić jako -----
+3. musimy odpytywać użytkownika cały czas o litery
+4. musimy zmniejszać życia użytkownika
+5. wyświetlić informacje o wygranej lub przegranej
+
+6. uwzglednij male i duze litery
+
+WERSJA 1
+Ustawiamy hasło w kodzie
+
+WERSJA 2
+Program losuje hasło z listy haseł
+
+WERSJA 3
+Podajemy hasło w konsoli, a program wymazuje je z konsoli i zamiast niego wyświetla -----
+"""
+
+haslo = 'masło'
+slowo = ''
+
+for i in range(len(haslo)):
+    slowo+='-'
+print(slowo)
+
+litera = input('wpisz litere: ')
+if litera in haslo:
+    print('tak')
+else:
+    print('nie')
+
+
+
+
+
+
+
+
+
